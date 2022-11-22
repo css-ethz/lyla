4acb8dadc96c82ac4b3891b28d3384893417cbd7
CristyGuzman <csolis@student.ethz.ch>
2022-11-22 14:45:56 +0100

    recenter tutorial

---
+++ src/components/Ocean.js
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import { GeoJSON } from 'react-leaflet';
+
+const Ocean = ({ geojson_data, key_id }) => {
+const geoJsonRef = useRef(null);
+const style = {
+            fillColor: "#1D212D",
+            weight: 1,
+            opacity: 1,
+            color: 'white',
+            dashArray: '1',
+            fillOpacity: 1
+        };
+
+return (
+    <GeoJSON
+        data={geojson_data}
+        key={key_id}
+        ref={geoJsonRef}
+        style={style}
+    />
+
+
+
+);
+
+};
+
+export default Ocean;
