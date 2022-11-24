1c7b8bc1cf8daa64e6f42cf8f291615fe1174850
CristyGuzman <csolis@student.ethz.ch>
2022-11-24 01:59:11 +0100

    activate tutorial by default

--- src/App.js
+++ src/App.js
@@ -161,7 +161,7 @@ function App() {
   const [occs, setOccs] = useState(null);
   const [numEvents, setNumEvents] = useState(2818);
   const [countryKey, setcountrykey] = useState('Latin America');
-  const [runTour, setRunTour] = useState(false);
+  const [runTour, setRunTour] = useState(true);
   const [heat, setheat] = useState(() => {
     var groups = filteredData_agg.reduce(function (r, row) {
       r[row.name_0] = ++r[row.name_0] || 1;
