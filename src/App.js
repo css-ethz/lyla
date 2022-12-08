e1f07fc4f7f584a18adae7bf5309d7edeb0d1a91
CristyGuzman <csolis@student.ethz.ch>
2022-12-08 15:26:29 +0100

    add image credits

--- src/App.js
+++ src/App.js
@@ -668,7 +668,8 @@ const ParentFunction = (e) => {
       
        
         {/* Use sizes relative to screen size */}
-        
+        <img src={require("./eth_logo_kurz_neg.png")} style={{height:'50px'}}/>
+      
         <h1 style={{ fontSize: "8vmin", marginLeft: "-11vw", marginRight: "1vw", marginTop: "-50pt" }}>{content['title'][lan]}</h1>
         
         <p style={{ fontSize: "15pt", marginLeft: "9vw", marginRight: "20vw", marginTop: "30vh" }}>
@@ -692,7 +693,7 @@ const ParentFunction = (e) => {
       
        
         
-
+        <p style={{opacity:"0.3",position:"absolute", bottom:"20pt", right: "22vmin"}}>Image by <a style={{color: "inherit"}} href="https://unsplash.com/@iequezada"> Isaac Quezada</a></p>
       </div>
       <div className='intro' style={{ marginTop: "10pt" }}>
         <h2 style={{marginLeft: "10pt",marginTop:"40pt"}}>{content['title'][lan]}</h2>
