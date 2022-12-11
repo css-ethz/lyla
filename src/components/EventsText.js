286fc1ebb6cec682c1e4c0c21ed62dc10cb7909d
CristyGuzman <csolis@student.ethz.ch>
2022-12-11 14:02:34 +0100

    translate events text

--- src/components/EventsText.js
+++ src/components/EventsText.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 
 
-const EventsText = ({ country, num_events }) => { 
+const EventsText = ({ country, num_events, content, lan }) => { 
     return (
         <div>
             <p className='num-events-title'>
-                {country !== 'Latin America' ? country : 'Latin America'}: {country !== 'Latin America' && num_events[country] > 0 ? num_events[country]: country !== 'Latin America' ? 0 : 2818} reported cases
+                {country !== 'Latin America' ? country : 'Latin America'}: {country !== 'Latin America' && num_events[country] > 0 ? num_events[country]: country !== 'Latin America' ? 0 : 2818} {content['events-text'][lan]}
             </p>
            
         </div>
